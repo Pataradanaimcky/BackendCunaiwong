e4b76ca7626c28bd77842461938c460999719847
Pataradanaimcky <116214675+Pataradanaimcky@users.noreply.github.com>
2022-11-02 09:41:37 +0700

    hgfds

--- dist/routes/cutded.router.js
+++ dist/routes/cutded.router.js
@@ -23,10 +23,26 @@ const rating_1 = require("../rating");
 exports.cutdedRouter = express_1.default.Router();
 exports.cutdedRouter.use(express_1.default.json());
 // GET
-exports.cutdedRouter.get("/", (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
+exports.cutdedRouter.get("/food", (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const foods = (yield database_service_1.collections.food.find({}).toArray());
         res.status(200).send(foods);
+    }
+    catch (error) {
+        if (error instanceof mongodb_1.MongoError) {
+            res.status(500).send(error.message);
+            return;
+        }
+        else {
+            res.status(500).send("unknown error");
+            return;
+        }
+    }
+}));
+exports.cutdedRouter.get("/review", (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const foodsreview = (yield database_service_1.collections.review.find({}).toArray());
+        res.status(200).send(foodsreview);
     }
     catch (error) {
         if (error instanceof mongodb_1.MongoError) {
@@ -45,10 +61,29 @@ exports.cutdedRouter.get("/:id", (req, res) => __awaiter(void 0, void 0, void 0,
     const query = { _id: new mongodb_1.ObjectId(id) };
 }));
 // POST
-exports.cutdedRouter.post("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+exports.cutdedRouter.post("/food", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const newFood = req.body;
         const result = yield database_service_1.collections.food.insertOne(newFood);
+        result
+            ? res.status(201).send(`Successfully created a new food with id ${result.insertedId}`)
+            : res.status(500).send("Failed to create a new food.");
+    }
+    catch (error) {
+        if (error instanceof mongodb_1.MongoError) {
+            res.status(400).send(error.message);
+            return;
+        }
+        else {
+            res.status(400).send("unknown error");
+            return;
+        }
+    }
+}));
+exports.cutdedRouter.post("/review", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const newReview = req.body;
+        const result = yield database_service_1.collections.review.insertOne(newReview);
         result
             ? res.status(201).send(`Successfully created a new food with id ${result.insertedId}`)
             : res.status(500).send("Failed to create a new food.");
